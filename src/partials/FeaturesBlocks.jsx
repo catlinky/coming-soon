45d4ba037bacf164eba2d65050c93abca987d034
Vincent Ri <vincent.ri@carsome.com>
2023-03-22 11:52:58 +0800

    a

--- src/partials/FeaturesBlocks.jsx
+++ src/partials/FeaturesBlocks.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import FeatBlockImg1 from '../images/block-1.png';
+import FeatBlockImg2 from '../images/block-1.png';
+import FeatBlockImg3 from '../images/block-1.png';
+import FeatBlockImg4 from '../images/block-1.png';
+import FeatBlockImg5 from '../images/block-1.png';
+import FeatBlockImg6 from '../images/block-1.png';
 
 function FeaturesBlocks() {
   return (
@@ -30,50 +35,29 @@ function FeaturesBlocks() {
               data-aos-anchor="[data-aos-id-blocks]"
             >
               <img
-                  className="max-w-full mx-auto md:max-w-none h-auto"
-                  src={FeatBlockImg1}
-                  width="80"
-                  alt="Features 01"
-                />
+                className="max-w-full mx-auto md:max-w-none h-auto"
+                src={FeatBlockImg1}
+                width="80"
+                alt="Features 01"
+              />
               <h4 className="h4 mb-2">Customizable Bio Link</h4>
               <p className="text-lg text-gray-400 text-center">
                 Showcase all of your important links in one place
               </p>
             </div>
 
-            {/* 2nd item */}
             <div
               className="relative flex flex-col items-center"
               data-aos="fade-up"
               data-aos-delay="100"
               data-aos-anchor="[data-aos-id-blocks]"
             >
-              <svg
-                className="w-16 h-16 mb-4"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <circle
-                  className="fill-current text-purple-600"
-                  cx="32"
-                  cy="32"
-                  r="32"
-                />
-                <path
-                  className="stroke-current text-purple-100"
-                  strokeWidth="2"
-                  strokeLinecap="square"
-                  d="M21 23h22v18H21z"
-                  fill="none"
-                  fillRule="evenodd"
-                />
-                <path
-                  className="stroke-current text-purple-300"
-                  d="M26 28h12M26 32h12M26 36h5"
-                  strokeWidth="2"
-                  strokeLinecap="square"
-                />
-              </svg>
+              <img
+                className="max-w-full mx-auto md:max-w-none h-auto"
+                src={FeatBlockImg2}
+                width="80"
+                alt="Features 02"
+              />
               <h4 className="h4 mb-2">Online Store</h4>
               <p className="text-lg text-gray-400 text-center">
                 Sell products directly from your bio link with our e-commerce
@@ -88,36 +72,12 @@ function FeaturesBlocks() {
               data-aos-delay="300"
               data-aos-anchor="[data-aos-id-blocks]"
             >
-              <svg
-                className="w-16 h-16 mb-4"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  className="fill-current text-purple-600"
-                  width="64"
-                  height="64"
-                  rx="32"
-                />
-                <g
-                  transform="translate(22 21)"
-                  strokeLinecap="square"
-                  strokeWidth="2"
-                  fill="none"
-                  fillRule="evenodd"
-                >
-                  <path
-                    className="stroke-current text-purple-100"
-                    d="M17 22v-6.3a8.97 8.97 0 003-6.569A9.1 9.1 0 0011.262 0 9 9 0 002 9v1l-2 5 2 1v4a2 2 0 002 2h4a5 5 0 005-5v-5"
-                  />
-                  <circle
-                    className="stroke-current text-purple-300"
-                    cx="13"
-                    cy="9"
-                    r="3"
-                  />
-                </g>
-              </svg>
+              <img
+                className="max-w-full mx-auto md:max-w-none h-auto"
+                src={FeatBlockImg3}
+                width="80"
+                alt="Features 03"
+              />
               <h4 className="h4 mb-2">Appointment Scheduling</h4>
               <p className="text-lg text-gray-400 text-center">
                 Allow clients to schedule appointments with you directly from
@@ -132,33 +92,12 @@ function FeaturesBlocks() {
               data-aos-delay="400"
               data-aos-anchor="[data-aos-id-blocks]"
             >
-              <svg
-                className="w-16 h-16 mb-4"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  className="fill-current text-purple-600"
-                  width="64"
-                  height="64"
-                  rx="32"
-                />
-                <g
-                  strokeLinecap="square"
-                  strokeWidth="2"
-                  fill="none"
-                  fillRule="evenodd"
-                >
-                  <path
-                    className="stroke-current text-purple-100"
-                    d="M29 42h10.229a2 2 0 001.912-1.412l2.769-9A2 2 0 0042 29h-7v-4c0-2.373-1.251-3.494-2.764-3.86a1.006 1.006 0 00-1.236.979V26l-5 6"
-                  />
-                  <path
-                    className="stroke-current text-purple-300"
-                    d="M22 30h4v12h-4z"
-                  />
-                </g>
-              </svg>
+              <img
+                className="max-w-full mx-auto md:max-w-none h-auto"
+                src={FeatBlockImg4}
+                width="80"
+                alt="Features 04"
+              />
               <h4 className="h4 mb-2">Analytics and Tracking</h4>
               <p className="text-lg text-gray-400 text-center">
                 Measure the success of your bio link and e-commerce store with
@@ -173,40 +112,12 @@ function FeaturesBlocks() {
               data-aos-delay="500"
               data-aos-anchor="[data-aos-id-blocks]"
             >
-              <svg
-                className="w-16 h-16 mb-4"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  className="fill-current text-purple-600"
-                  width="64"
-                  height="64"
-                  rx="32"
-                />
-                <g
-                  transform="translate(21 22)"
-                  strokeLinecap="square"
-                  strokeWidth="2"
-                  fill="none"
-                  fillRule="evenodd"
-                >
-                  <path
-                    className="stroke-current text-purple-300"
-                    d="M17 2V0M19.121 2.879l1.415-1.415M20 5h2M19.121 7.121l1.415 1.415M17 8v2M14.879 7.121l-1.415 1.415M14 5h-2M14.879 2.879l-1.415-1.415"
-                  />
-                  <circle
-                    className="stroke-current text-purple-300"
-                    cx="17"
-                    cy="5"
-                    r="3"
-                  />
-                  <path
-                    className="stroke-current text-purple-100"
-                    d="M8.86 1.18C3.8 1.988 0 5.6 0 10c0 5 4.9 9 11 9a10.55 10.55 0 003.1-.4L20 21l-.6-5.2a9.125 9.125 0 001.991-2.948"
-                  />
-                </g>
-              </svg>
+              <img
+                className="max-w-full mx-auto md:max-w-none h-auto"
+                src={FeatBlockImg5}
+                width="80"
+                alt="Features 05"
+              />
               <h4 className="h4 mb-2">Social Media Integration</h4>
               <p className="text-lg text-gray-400 text-center">
                 Connect your social media accounts to your bio link for a
@@ -220,43 +131,12 @@ function FeaturesBlocks() {
               data-aos-delay="200"
               data-aos-anchor="[data-aos-id-blocks]"
             >
-              <svg
-                className="w-16 h-16 mb-4"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  className="fill-current text-purple-600"
-                  width="64"
-                  height="64"
-                  rx="32"
-                />
-                <g
-                  transform="translate(21 21)"
-                  strokeLinecap="square"
-                  strokeWidth="2"
-                  fill="none"
-                  fillRule="evenodd"
-                >
-                  <ellipse
-                    className="stroke-current text-purple-300"
-                    cx="11"
-                    cy="11"
-                    rx="5.5"
-                    ry="11"
-                  />
-                  <path
-                    className="stroke-current text-purple-100"
-                    d="M11 0v22M0 11h22"
-                  />
-                  <circle
-                    className="stroke-current text-purple-100"
-                    cx="11"
-                    cy="11"
-                    r="11"
-                  />
-                </g>
-              </svg>
+              <img
+                className="max-w-full mx-auto md:max-w-none h-auto"
+                src={FeatBlockImg6}
+                width="80"
+                alt="Features 06"
+              />
               <h4 className="h4 mb-2">And More...</h4>
               <p className="text-lg text-gray-400 text-center">
                 We have lot of new things coming for you.
