2147f340b00b04500ab20a279c56ae06cc13b0c8
Vincent Ri <vincent.ri@carsome.com>
2023-03-22 11:43:52 +0800

    a

--- src/partials/FeaturesBlocks.jsx
+++ src/partials/FeaturesBlocks.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import FeatBlockImg1 from '../images/block-1.png';
 
 function FeaturesBlocks() {
   return (
@@ -28,32 +29,12 @@ function FeaturesBlocks() {
               data-aos="fade-up"
               data-aos-anchor="[data-aos-id-blocks]"
             >
-              <svg
-                className="w-16 h-16 mb-4"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  className="fill-current text-purple-600"
-                  width="64"
-                  height="64"
-                  rx="32"
+              <img
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src={FeatBlockImg1}
+                  width="80"
+                  alt="Features 01"
                 />
-                <path
-                  className="stroke-current text-purple-100"
-                  d="M30 39.313l-4.18 2.197L27 34.628l-5-4.874 6.91-1.004L32 22.49l3.09 6.26L42 29.754l-3 2.924"
-                  strokeLinecap="square"
-                  strokeWidth="2"
-                  fill="none"
-                  fillRule="evenodd"
-                />
-                <path
-                  className="stroke-current text-purple-300"
-                  d="M43 42h-9M43 37h-9"
-                  strokeLinecap="square"
-                  strokeWidth="2"
-                />
-              </svg>
               <h4 className="h4 mb-2">Customizable Bio Link</h4>
               <p className="text-lg text-gray-400 text-center">
                 Showcase all of your important links in one place
